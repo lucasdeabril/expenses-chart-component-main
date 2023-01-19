e6a62f85e0be787378ebf4f599a84e2dc301bc79
lucasdeabril <118875218+lucasdeabril@users.noreply.github.com>
2023-01-19 15:42:33 -0400

    Add files via upload

---
+++ expenses-chart-component-main/script.js
@@ -0,0 +1,110 @@
+
+
+function altura(){
+    var alt = document.getElementById('alt').value
+
+    var a = 5
+    var n = parseInt (alt)
+    var soma = a + n
+    
+    alert(`ok ${parseInt(n)} ${a} ${soma} ${alt}`)
+    console.log( n + a + soma + alt)
+}
+
+
+
+function liga1(){
+    // alert("ola")
+    let tag1 = document.getElementById('pog1')
+
+    tag1.style.backgroundColor = 'black';
+    
+}
+function desliga1(){
+    let tag1 = document.getElementById('pog1')
+
+    tag1.style.backgroundColor = 'hsl(0, 0%, 96%)';
+
+}
+
+function liga2(){
+    // alert("ola")
+    let tag2 = document.getElementById('pog2')
+
+    tag2.style.backgroundColor = 'black';
+    
+}
+function desliga2(){
+    let tag2 = document.getElementById('pog2')
+
+    tag2.style.backgroundColor = 'hsl(0, 0%, 96%)';
+
+}
+function liga3(){
+    // alert("ola")
+    let tag3 = document.getElementById('pog3')
+
+    tag3.style.backgroundColor = 'black';
+    
+}
+function desliga3(){
+    let tag2 = document.getElementById('pog3')
+
+    tag2.style.backgroundColor = 'hsl(0, 0%, 96%)';
+
+}
+function liga4(){
+    // alert("ola")
+    let tag4 = document.getElementById('pog4')
+
+    tag4.style.backgroundColor = 'black';
+    
+}
+function desliga4(){
+    let tag2 = document.getElementById('pog4')
+
+    tag2.style.backgroundColor = 'hsl(0, 0%, 96%)';
+
+}
+function liga5(){
+    // alert("ola")
+    let tag5 = document.getElementById('pog5')
+
+    tag5.style.backgroundColor = 'black';
+    
+}
+function desliga5(){
+    let tag2 = document.getElementById('pog5')
+
+    tag2.style.backgroundColor = 'hsl(0, 0%, 96%)';
+
+}
+function liga6(){
+    // alert("ola")
+    let tag6 = document.getElementById('pog6')
+
+    tag6.style.backgroundColor = 'black';
+    
+}
+function desliga6(){
+    let tag2 = document.getElementById('pog6')
+
+    tag2.style.backgroundColor = 'hsl(0, 0%, 96%)';
+
+}
+function liga7(){
+    // alert("ola")
+    let tag7 = document.getElementById('pog7')
+
+    tag7.style.backgroundColor = 'black';
+    
+}
+function desliga7(){
+    let tag2 = document.getElementById('pog7')
+
+    tag2.style.backgroundColor = 'hsl(0, 0%, 96%)';
+
+}
+
+
+
